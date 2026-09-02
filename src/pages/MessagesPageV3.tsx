c13c4494a142cd4f81675e9b00eb065ca3e18894
taufx98 <taufxacc11@gmail.com>
2026-09-02 12:44:09 +0700

    chore(cleanup): remove redundant chat route wrapper

--- src/pages/MessagesPageV3.tsx
+++
@@ -1,4 +0,0 @@
-import { useApp } from '@/store/AppContext';
-import { MessagesPageV6 } from './MessagesPageV6';
-import { AdminChatConsolePage } from './AdminChatConsolePage';
-export function MessagesPageV3(){const { user } = useApp(); return user?.role === 'admin' ? <AdminChatConsolePage/> : <MessagesPageV6/>;}
