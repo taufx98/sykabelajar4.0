9c17fb23ede5ca2bf7b26bb2826dba0b81c997de
taufx98 <taufxacc11@gmail.com>
2026-09-04 02:07:56 +0700

    feat: start isolated cache auth lifecycle

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { startCacheAuthLifecycle } from './lib/cacheAuthLifecycle';
 import './index.css';
+
+startCacheAuthLifecycle();
 
 const container = document.getElementById('page-root') ?? document.getElementById('root');
 
