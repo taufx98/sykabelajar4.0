bbecf97929e2de142a4205711b0c6788a7bc1507
taufx98 <taufxacc11@gmail.com>
2026-09-06 14:56:11 +0700

    security: lock internal RPC execution

---
+++ supabase/migrations/20260906075700_phase_13_remove_public_execute_internal_rpcs.sql
@@ -0,0 +1,3 @@
+revoke execute on function public.issue_collective_certificate_after_payment(uuid) from public;
+revoke execute on function public.regenerate_collective_participant_password(uuid) from public;
+revoke execute on function public.resolve_daily_checkin_reward(date, integer) from public;
