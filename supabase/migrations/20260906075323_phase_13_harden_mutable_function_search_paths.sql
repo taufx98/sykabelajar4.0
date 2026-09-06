6e4573753c0b5e9c0a9986244bd0f35e9d339e70
taufx98 <taufxacc11@gmail.com>
2026-09-06 14:56:15 +0700

    security: harden mutable function search paths

---
+++ supabase/migrations/20260906075323_phase_13_harden_mutable_function_search_paths.sql
@@ -0,0 +1,2 @@
+alter function public.fetch_and_save_supabase_logs() set search_path = public, extensions, pg_catalog;
+alter function public.sync_participant_updated_at() set search_path = public, pg_catalog;
